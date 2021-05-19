f4ded4d5c4e960c5e48e7970ad73efe18a4dd108
Fice <Zomfice@163.com>
2021-05-19 10:21:46 +0800

    Network Log Target

---
+++ DnpLog/Classes/Protocol/DnpLogManager.swift
@@ -0,0 +1,13 @@
+//
+//  DnpLogManager.swift
+//  DnpLog
+//
+//  Created by Fice on 2021/5/19.
+//
+
+import Foundation
+
+
+public class DnpLogManager: NSObject {
+
+}
---
+++ Example/Pods/Target Support Files/DnpLog/DnpLog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DnpLog : NSObject
+@end
+@implementation PodsDummy_DnpLog
+@end
